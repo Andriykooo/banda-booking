70dcb216eda3b9cf48a7c80504d1a0e45064284e
AndriiBoichuk <boichukandri9@gmail.com>
2022-09-17 14:34:19 +0300

    centered cards

--- pages/rooms.jsx
+++ pages/rooms.jsx
@@ -10,9 +10,9 @@ const Rooms = () => {
   return (
     <Container maxWidth='md'>
       <Grid container spacing={2} p={2}>
-        <Grid item xs={8}>
+        <Grid item xs={6}>
           <Card
-            sx={{ maxWidth: 345 }}
+            sx={{ maxWidth: '100%', boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px' }}
             onClick={() => {
               console.log('open');
             }}
@@ -31,35 +31,7 @@ const Rooms = () => {
                 <List>
                   <ListItem disablePadding>
                     <ListItemText primary='10:00' />
-                    <Chip label='Zaynyato' />
-                  </ListItem>
-                </List>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        </Grid>
-        <Grid item xs={8}>
-          <Card
-            sx={{ maxWidth: 345 }}
-            onClick={() => {
-              console.log('open');
-            }}
-          >
-            <CardActionArea>
-              <CardMedia
-                component='img'
-                height='140'
-                image='https://media.npr.org/assets/img/2017/09/12/macaca_nigra_self-portrait-3e0070aa19a7fe36e802253048411a38f14a79f8-s1100-c50.jpg'
-                alt='green iguana'
-              />
-              <CardContent>
-                <Typography gutterBottom variant='h5' component='div'>
-                  Room: 1
-                </Typography>
-                <List>
-                  <ListItem disablePadding>
-                    <ListItemText primary='10:00' />
-                    <Chip label='Zaynyato' />
+                    <Chip label='Reserved' />
                   </ListItem>
                 </List>
               </CardContent>
