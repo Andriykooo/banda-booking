4b74d86dcb5cbb90b5081221ed0bc4dcf69413d7
valentinYelkin <valentinyelkin2022@gmail.com>
2022-09-18 13:20:55 +0300

    add loading bar

--- pages/kitchen/index.jsx
+++ pages/kitchen/index.jsx
@@ -65,4 +65,10 @@ const Kitchen = () => {
   );
 };
 
+export async function getServerSideProps(context) {
+    return {
+        props: {}, // will be passed to the page component as props
+    }
+}
+
 export default Kitchen;
